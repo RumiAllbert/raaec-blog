5920d112a93e06ab700caf588e40853e05b6e967
Rumi Allbert <rumiallbert@gmail.com>
2024-03-03 16:02:29 -0500

    Add dark theme support to Page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
 import { HeroParallax, products } from "../components/ui/hero-parallax";
 
-// export function HeroParallaxDemo()  {
-//   return <HeroParallax products={products} />;
-// }
-
-// export default HeroParallaxDemo;
 export default function Page() {
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    setTheme("dark");
+  }, []);
+
   return <HeroParallax products={products} />;
 }
-
